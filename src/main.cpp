cfebc0c20a1cce30441badb3c5636ca6cd0089dc
Christian Ponte <christian.ponte@udc.es>
2018-04-10 19:49:41 +0200

    Added proper print messages

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,25 @@
 #include "ThreadSpawner.h"
 #include <iostream>
 
+void print_frequencies(const std::vector<unsigned short> &cores, const std::vector<double> &frequencies,
+                       const std::string &print_prefix) {
+    for (int i = 0; i < cores.size(); i++) {
+        std::cout << print_prefix << "Core " + std::to_string(cores[i]) + ": " + std::to_string(frequencies[i]) + " MHz\n";
+    }
+}
+
 int main(int argc, const char **argv) {
     argh::parser args({"-u", "--update-time"});
     args.parse(argc, argv);
 
-    if (args[0].empty()){
-        std::cout << "Error" << std::endl;
+    // Check if there is any argument or asked to print help
+    if (args[{"h", "--help"}] || args[1].empty()){
+        if (args[{"h", "--help"}]){
+            std::cout << "Usage:\n";
+        } else {
+            std::cout << "Missing arguments:\n";
+        }
+        std::cout << args[0] << " [--update-time <milliseconds>] <core1 id> [<core2 id> ...]" << std::endl;
         return 0;
     }
 
@@ -18,10 +31,6 @@ int main(int argc, const char **argv) {
     while (!args[i].empty()) {
         cores.push_back((unsigned short) std::stoi(args[i++]));
     }
-    std::cout << cores.size() << std::endl;
-    for (auto c : cores){
-        std::cout << c << std::endl;
-    }
 
     // Read frequency update time
     unsigned freq_update_time;
@@ -30,21 +39,14 @@ int main(int argc, const char **argv) {
     ThreadSpawner spawner;
     FrequencyReader *reader = FrequencyReaderBuilder::build(cores);
 
-    auto frequencies = reader->get_frequencies();
-    for (i = 0; i < cores.size(); i++) {
-        std::cout << "Core " + std::to_string(cores[i]) + ": " + std::to_string(frequencies[i]) + " MHz" << std::endl;
-    }
-
+    std::cout << "Initial frequencies:\n";
+    print_frequencies(cores, reader->get_frequencies(), "\t");
     for (unsigned short c : cores) {
+        std::cout << "Stressing core " << c << " and sleeping for " << freq_update_time << " ms...\n";
         spawner.spawn(c);
         std::this_thread::sleep_for(std::chrono::milliseconds(freq_update_time));
-
-        frequencies = reader->get_frequencies();
-        for (i = 0; i < cores.size(); i++) {
-            std::cout << "Core " + std::to_string(cores[i]) + ": " + std::to_string(frequencies[i]) + " MHz"
-                      << std::endl;
-        }
+        print_frequencies(cores, reader->get_frequencies(), "\t");
     }
-
+    std::cout << "\nTest finished!" << std::endl;
     return 0;
 }
